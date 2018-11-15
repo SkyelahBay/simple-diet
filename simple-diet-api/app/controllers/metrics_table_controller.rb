ca57120bd9f6f0c96746a91d56388bdad0cfc71c
Skyelah <skyelahbay@gmail.com>
2018-11-14 23:05:54 -0600

    fixed missing commas on metrics_table_controller

--- simple-diet-api/app/controllers/metrics_table_controller.rb
+++ simple-diet-api/app/controllers/metrics_table_controller.rb
@@ -2,8 +2,8 @@ class MetricsTableController < ApplicationController
   def index
     render json: {
       metrics: {
-        total_calories_burned: current_user.metrics_table.first.total_calories_burned
-        total_calories_gained: current_user.metrics_table.first.total_calories_gained
+        total_calories_burned: current_user.metrics_table.first.total_calories_burned,
+        total_calories_gained: current_user.metrics_table.first.total_calories_gained,
         record_calories_burned: current_user.metrics_table.first.record_calories_burned
       }
     }
