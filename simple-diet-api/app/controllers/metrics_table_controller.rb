fde975db8e1cdf9eeea3f8035ea75d0b0b4a5dcb
Skyelah <skyelahbay@gmail.com>
2018-11-14 23:10:27 -0600

    troubleshooting metrics_controller

--- simple-diet-api/app/controllers/metrics_table_controller.rb
+++ simple-diet-api/app/controllers/metrics_table_controller.rb
@@ -1,5 +1,6 @@
 class MetricsTableController < ApplicationController
   def index
+
     render json: {
       metrics: {
         total_calories_burned: current_user.metrics_table.first.total_calories_burned,
